4b56d431e859e970a5cfb396e30767ca3688b5e6
Bairdma <e12813726@gmail.com>
2021-10-30 14:09:56 +0800

    參數改為調用常數

--- index.js
+++ index.js
@@ -20,25 +20,25 @@ function bindBtnEvent() {
         for (var i = 0; i <= class_count.length - 1; i++) {
             let this_class = class_count[i]
             let class_top = this_class.getElementsByClassName(elementProperty.TIMING_SLT)[0].value
-            let command_count = this_class.getElementsByClassName("command");
+            let command_count = this_class.getElementsByClassName(elementProperty.COMMAND);
 
             repo_string = repo_string + `${class_top}{ \n`;
 
             for (var i = 0; i <= command_count.length - 1; i++) {
                 let this_commmand = command_count[i];
-                let this_commmand_div = this_commmand.getElementsByClassName("command_select_div")[0];
-                let this_target_slt_div = this_commmand.getElementsByClassName("target_select_div")[0];
+                let this_commmand_div = this_commmand.getElementsByClassName(elementProperty.COMMAND_SLT_DIV)[0];
+                let this_target_slt_div = this_commmand.getElementsByClassName(elementProperty.TARGET_SLT_DIV)[0];
 
-                let this_command_slt_value = this_commmand.getElementsByClassName("command_slt")[0].value;
-                let this_target_slt_value = this_commmand_div.getElementsByClassName("target_slt")[0] && this_commmand_div.getElementsByClassName("target_slt")[0].value;
-                let this_command_text_1_value = this_commmand_div.getElementsByClassName("command_text_1")[0] && this_commmand_div.getElementsByClassName("command_text_1")[0].value;
-                let this_command_text_2_value = this_commmand_div.getElementsByClassName("command_text_2")[0] && this_commmand_div.getElementsByClassName("command_text_2")[0].value;
+                let this_command_slt_value = this_commmand.getElementsByClassName(elementProperty.COMMAND_SLT)[0].value;
+                let this_target_slt_value = this_commmand_div.getElementsByClassName(elementProperty.TARGET_SLT)[0] && this_commmand_div.getElementsByClassName(elementProperty.TARGET_SLT)[0].value;
+                let this_command_text_1_value = this_commmand_div.getElementsByClassName(elementProperty.COMMAND_TEXT_1)[0] && this_commmand_div.getElementsByClassName(elementProperty.COMMAND_TEXT_1)[0].value;
+                let this_command_text_2_value = this_commmand_div.getElementsByClassName(elementProperty.COMMAND_TEXT_2)[0] && this_commmand_div.getElementsByClassName(elementProperty.COMMAND_TEXT_2)[0].value;
 
                 let this_target_in_one_value, this_target_in_two_value, this_target_text_value
                 if (this_target_slt_div) {
-                    this_target_in_one_value = this_target_slt_div.getElementsByClassName("target_in_one")[0] && this_target_slt_div.getElementsByClassName("target_in_one")[0].value;
-                    this_target_in_two_value = this_target_slt_div.getElementsByClassName("target_in_two")[0] && this_target_slt_div.getElementsByClassName("target_in_two")[0].value;
-                    this_target_text_value = this_target_slt_div.getElementsByClassName("target_text")[0] && this_target_slt_div.getElementsByClassName("target_text")[0].value;
+                    this_target_in_one_value = this_target_slt_div.getElementsByClassName(elementProperty.TARGET_IN_ONE)[0] && this_target_slt_div.getElementsByClassName(elementProperty.TARGET_IN_ONE)[0].value;
+                    this_target_in_two_value = this_target_slt_div.getElementsByClassName(elementProperty.TARGET_IN_TWO)[0] && this_target_slt_div.getElementsByClassName(elementProperty.TARGET_IN_TWO)[0].value;
+                    this_target_text_value = this_target_slt_div.getElementsByClassName(elementProperty.TARGET_TEXT)[0] && this_target_slt_div.getElementsByClassName(elementProperty.TARGET_TEXT)[0].value;
                 }
                 repo_string = repo_string + `\t `;
                 if (this_target_slt_value) {
