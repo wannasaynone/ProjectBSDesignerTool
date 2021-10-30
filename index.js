012e9f22573693d36435fb30f335f994c9d25056
Bairdma <e12813726@gmail.com>
2021-10-30 13:52:15 +0800

    完成export按鈕作動

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 
 createContainer("load");
 
-if (typeof a !== 'undefined') {
-    console.log('aaaa')
-} else {
-    console.log("ddddd")
-}
 bindBtnEvent();
 function bindBtnEvent() {
 
@@ -15,5 +10,49 @@ function bindBtnEvent() {
         createContainer("build");
     })
 
+    let repo_btn = document.getElementById(elementProperty.REPO_BTN);
 
+    repo_btn.addEventListener("click", function (e) {
+        let repo_string = "";
+        let export_text = document.getElementById(elementProperty.EXPORT_TEXT);
+        let class_count = document.getElementsByClassName(elementProperty.CONTENT_BLOCK);
+
+        for (var i = 0; i <= class_count.length - 1; i++) {
+            let this_class = class_count[i]
+            let class_top = this_class.getElementsByClassName(elementProperty.TIMING_SLT)[0].value
+            let command_count = this_class.getElementsByClassName("command");
+
+            repo_string = repo_string + `${class_top}{ \n\t`;
+
+            for (var i = 0; i <= command_count.length - 1; i++) {
+                let this_commmand = command_count[i];
+                let this_commmand_div = this_commmand.getElementsByClassName("command_select_div")[0];
+                let this_target_slt_div = this_commmand.getElementsByClassName("target_select_div")[0];
+
+                let this_command_slt_value = this_commmand.getElementsByClassName("command_slt")[0].value;
+                let this_target_slt_value = this_commmand_div.getElementsByClassName("target_slt")[0].value;
+                let this_command_text_1_value = this_commmand_div.getElementsByClassName("command_text_1")[0].value;
+                let this_command_text_2_value = this_commmand_div.getElementsByClassName("command_text_2")[0].value;
+
+                let this_target_in_one_value, this_target_in_two_value, this_target_text_value
+                if (this_target_slt_div) {
+                    this_target_in_one_value = this_target_slt_div.getElementsByClassName("target_in_one")[0] && this_target_slt_div.getElementsByClassName("target_in_one")[0].value;
+                    this_target_in_two_value = this_target_slt_div.getElementsByClassName("target_in_two")[0] && this_target_slt_div.getElementsByClassName("target_in_two")[0].value;
+                    this_target_text_value = this_target_slt_div.getElementsByClassName("target_text")[0] && this_target_slt_div.getElementsByClassName("target_text")[0].value;
+                }
+                repo_string = repo_string + `${this_command_slt_value}(${this_target_slt_value}`;
+                if (this_target_in_one_value) { // 選擇對象指令有出現第三排
+                    repo_string = repo_string + `(${this_target_in_one_value},${this_target_in_two_value},${this_target_text_value})`;
+                } else if (this_target_text_value) {
+                    repo_string = repo_string + `(${this_target_text_value})`;
+                }
+                repo_string = repo_string + `,${this_command_text_1_value},${this_command_text_2_value}); \n`;
+                //AddStatus(SelectOther (Opponent, Manual, 1), string, string)
+            }
+
+            repo_string = repo_string + ` \n }`;
+        }
+
+        export_text.value = repo_string;
+    })
 }
