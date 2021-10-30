8d13f66ade8e2a7dab9a4e0f08e07c91653d685c
Bairdma <e12813726@gmail.com>
2021-10-30 14:19:47 +0800

    排版

--- index.js
+++ index.js
@@ -17,12 +17,15 @@ function bindBtnEvent() {
         let export_text = document.getElementById(elementProperty.EXPORT_TEXT);
         let class_count = document.getElementsByClassName(elementProperty.CONTENT_BLOCK);
 
-        for (var i = 0; i <= class_count.length - 1; i++) {
-            let this_class = class_count[i]
+        for (var x = 0; x <= class_count.length - 1; x++) {
+            let this_class = class_count[x]
             let class_top = this_class.getElementsByClassName(elementProperty.TIMING_SLT)[0].value
             let command_count = this_class.getElementsByClassName(elementProperty.COMMAND);
 
-            repo_string = repo_string + `${class_top}{ \n`;
+            if (repo_string !== "") {
+                repo_string = repo_string + `\n\n`;
+            }
+            repo_string = repo_string + `${class_top}{`;
 
             for (var i = 0; i <= command_count.length - 1; i++) {
                 let this_commmand = command_count[i];
@@ -40,7 +43,7 @@ function bindBtnEvent() {
                     this_target_in_two_value = this_target_slt_div.getElementsByClassName(elementProperty.TARGET_IN_TWO)[0] && this_target_slt_div.getElementsByClassName(elementProperty.TARGET_IN_TWO)[0].value;
                     this_target_text_value = this_target_slt_div.getElementsByClassName(elementProperty.TARGET_TEXT)[0] && this_target_slt_div.getElementsByClassName(elementProperty.TARGET_TEXT)[0].value;
                 }
-                repo_string = repo_string + `\t `;
+                repo_string = repo_string + `\n\t `;
                 if (this_target_slt_value) {
                     repo_string = repo_string + `${this_command_slt_value}(${this_target_slt_value}`;
                 } else {
