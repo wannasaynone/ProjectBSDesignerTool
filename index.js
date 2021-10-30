85133f114e074da8d933ed502acfc5ca0d19ecbd
Bairdma <e12813726@gmail.com>
2021-10-30 14:06:28 +0800

    修正取不到值時的處理方式

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function bindBtnEvent() {
             let class_top = this_class.getElementsByClassName(elementProperty.TIMING_SLT)[0].value
             let command_count = this_class.getElementsByClassName("command");
 
-            repo_string = repo_string + `${class_top}{ \n\t`;
+            repo_string = repo_string + `${class_top}{ \n`;
 
             for (var i = 0; i <= command_count.length - 1; i++) {
                 let this_commmand = command_count[i];
@@ -30,9 +30,9 @@ function bindBtnEvent() {
                 let this_target_slt_div = this_commmand.getElementsByClassName("target_select_div")[0];
 
                 let this_command_slt_value = this_commmand.getElementsByClassName("command_slt")[0].value;
-                let this_target_slt_value = this_commmand_div.getElementsByClassName("target_slt")[0].value;
-                let this_command_text_1_value = this_commmand_div.getElementsByClassName("command_text_1")[0].value;
-                let this_command_text_2_value = this_commmand_div.getElementsByClassName("command_text_2")[0].value;
+                let this_target_slt_value = this_commmand_div.getElementsByClassName("target_slt")[0] && this_commmand_div.getElementsByClassName("target_slt")[0].value;
+                let this_command_text_1_value = this_commmand_div.getElementsByClassName("command_text_1")[0] && this_commmand_div.getElementsByClassName("command_text_1")[0].value;
+                let this_command_text_2_value = this_commmand_div.getElementsByClassName("command_text_2")[0] && this_commmand_div.getElementsByClassName("command_text_2")[0].value;
 
                 let this_target_in_one_value, this_target_in_two_value, this_target_text_value
                 if (this_target_slt_div) {
@@ -40,17 +40,34 @@ function bindBtnEvent() {
                     this_target_in_two_value = this_target_slt_div.getElementsByClassName("target_in_two")[0] && this_target_slt_div.getElementsByClassName("target_in_two")[0].value;
                     this_target_text_value = this_target_slt_div.getElementsByClassName("target_text")[0] && this_target_slt_div.getElementsByClassName("target_text")[0].value;
                 }
-                repo_string = repo_string + `${this_command_slt_value}(${this_target_slt_value}`;
+                repo_string = repo_string + `\t `;
+                if (this_target_slt_value) {
+                    repo_string = repo_string + `${this_command_slt_value}(${this_target_slt_value}`;
+                } else {
+                    repo_string = repo_string + `${this_command_slt_value}(`;
+                }
+
                 if (this_target_in_one_value) { // 選擇對象指令有出現第三排
                     repo_string = repo_string + `(${this_target_in_one_value},${this_target_in_two_value},${this_target_text_value})`;
                 } else if (this_target_text_value) {
                     repo_string = repo_string + `(${this_target_text_value})`;
                 }
-                repo_string = repo_string + `,${this_command_text_1_value},${this_command_text_2_value}); \n`;
+                if (this_target_slt_value) {
+                    repo_string = repo_string + `,`
+                }
+                if (this_command_text_1_value) {
+                    repo_string = repo_string + `${this_command_text_1_value}`;
+                }
+
+                if (this_command_text_2_value) {
+                    repo_string = repo_string + `,${this_command_text_2_value}); `;
+                } else {
+                    repo_string = repo_string + `); `;
+                }
                 //AddStatus(SelectOther (Opponent, Manual, 1), string, string)
             }
 
-            repo_string = repo_string + ` \n }`;
+            repo_string = repo_string + ` \n } `;
         }
 
         export_text.value = repo_string;
